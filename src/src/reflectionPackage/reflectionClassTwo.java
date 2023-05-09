8d7362b4bd681ed44b3e088cbf29e989883daa7e
sanujakoralage <45386915+sanujakoralage@users.noreply.github.com>
2023-05-09 09:59:16 +0100

    Reflection class two

---
+++ src/src/reflectionPackage/reflectionClassTwo.java
@@ -0,0 +1,13 @@
+package reflectionPackage;
+
+
+/**
+ * Gets the object from the area class
+ * */
+public class reflectionClassTwo {
+	public static void main(String[] args) {
+		Area a = new Area();
+		System.out.println("Area = " + a);
+
+	}
+}
