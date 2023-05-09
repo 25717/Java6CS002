2031baf6171cca860f7fb7830b7ab5d53cc94a98
sanujakoralage <45386915+sanujakoralage@users.noreply.github.com>
2023-05-09 05:40:21 +0100

    Data clumps bad smell refactored

--- src/src/base/Location.java
+++ src/src/base/Location.java
@@ -11,21 +11,66 @@ import java.io.InputStreamReader;
  */
 
 public class Location extends SpacePlace {
+	
+	private LocationCoordinates coordinates;
+	
+	public Location(int r, int c) {
+		this.coordinates = new LocationCoordinates(r, c);
+	}
+	
+	public Location(int r, int c, LocationCoordinates.DIRECTION d) {
+		this.coordinates = new LocationCoordinates(r, c, d);
+	}
+
+	public String toString() {
+		return coordinates.toString();
+	}
+	//extract method refactoring
+	public void drawGridLines(Graphics g) {
+		drawHorizontalGridLines(g);
+		drawVerticalGridLines(g);
+	}
+	
+	private void drawHorizontalGridLines(Graphics g) {
+		g.setColor(Color.LIGHT_GRAY);
+		for (int tmp = 0; tmp <= 7; tmp++) {
+			g.drawLine(20, 20 + tmp * 20, 180, 20 + tmp * 20);
+		}
+	}
+	
+	private void drawVerticalGridLines(Graphics g) {
+		for (int see = 0; see <= 8; see++) {
+			g.drawLine(20 + see * 20, 20, 20 + see * 20, 160);
+		}
+	}
+	
+	//Divergent change
+	public static int getInt() {
+		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		do {
+			try {
+				return Integer.parseInt(r.readLine());
+			} catch (Exception e) {
+			}
+		} while (true);
+	}
+}
+class LocationCoordinates {
 	public int c; //column
 	public int r; //rows
 	public DIRECTION d; //direction
-	public int tmp;
-
+	
 	public enum DIRECTION {
 		VERTICAL, HORIZONTAL
 	};
+	
 
-	public Location(int r, int c) {
+	public LocationCoordinates(int r, int c) {
 		this.r = r;
 		this.c = c;
 	}
 
-	public Location(int r, int c, DIRECTION d) {
+	public LocationCoordinates(int r, int c, DIRECTION d) {
 		this(r, c);
 		this.d = d;
 	}
@@ -41,35 +86,4 @@ public class Location extends SpacePlace {
 		}
 	}
 	
-// Extract method refactoring
-	public void drawGridLines(Graphics g) {
-		
-		drawHorizontalGridLines(g);
-		drawVerticalGridLines(g);
-	}
-	
-	private void drawHorizontalGridLines(Graphics g) {
-		g.setColor(Color.LIGHT_GRAY);
-		for (tmp = 0; tmp <= 7; tmp++) {
-			g.drawLine(20, 20 + tmp * 20, 180, 20 + tmp * 20);
-		}
-	}
-	
-	private void drawVerticalGridLines(Graphics g) {
-		for (int see = 0; see <= 8; see++) {
-			g.drawLine(20 + see * 20, 20, 20 + see * 20, 160);
-		}
-	}
-	
-	
-////// DIVERGENT CHANGEEEE>>>////
-	public static int getInt() {
-		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-		do {
-			try {
-				return Integer.parseInt(r.readLine());
-			} catch (Exception e) {
-			}
-		} while (true);
-	}
 }
