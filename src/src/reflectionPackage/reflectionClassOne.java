791f40ee05d606f79b5b34aa02ddf1cc97e1dbd8
sanujakoralage <45386915+sanujakoralage@users.noreply.github.com>
2023-05-09 09:57:55 +0100

    reflection class one

---
+++ src/src/reflectionPackage/reflectionClassOne.java
@@ -0,0 +1,12 @@
+package reflectionPackage;
+
+public class reflectionClassOne {
+
+	public static void main(String[] args) {
+		Area a = new Area();
+		a.calculateArea();
+		System.out.println("Area = " + a);
+
+	}
+
+}
