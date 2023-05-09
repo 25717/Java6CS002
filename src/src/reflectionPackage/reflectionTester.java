0abde99076a2f8cdb5177d69c99553c623b708ab
sanujakoralage <45386915+sanujakoralage@users.noreply.github.com>
2023-05-09 10:01:27 +0100

    Launcher reflection unit testing

--- src/src/reflectionPackage/reflectionTester.java
+++ src/src/reflectionPackage/reflectionTester.java
@@ -1,16 +1,21 @@
 package reflectionPackage;
 
 import java.lang.reflect.*;
+import java.util.ArrayList;
 public class reflectionTester {
 
 	public static void main(String[] args) {
-		reflectionOne area = new reflectionOne(4.0, 5.0);
+		Area area = new Area(4.0, 5.0);
 		 runResilienceTesting(area);
+		 runLauncherTesting(area);
  
 	}
-	
-	//Reselience
-    public static void runResilienceTesting(reflectionOne rect) {
+	/*
+     * This method checks for the exception. If one of the message is "NoSuchMethodException", "SecurityException" or "IllegalAccessException"
+     * it will print out an error message with the exception. If not the 
+     * */
+	//Resilience
+    public static void runResilienceTesting(Area rect) {
     	
     	// Class<?>[] areaClass= new Class<?>[] {reflectionOne.class};
         
@@ -28,8 +33,37 @@ public class reflectionTester {
             System.out.println("Resilience test passed."); //message
         }
     }
-
+    
+    
+    /**
+     * 
+     * @param rect
+     */
     //launcher
+    public static void runLauncherTesting(Area rect) {
+    	Class<?> areaClass = rect.getClass(); //object using array list
+    	//Method method = areaClass.getMethod();
+    	//method.setAccessible(true);
+        //method.invoke(rect);
+    	ArrayList<Method> check = new ArrayList<>();//list
+    	Method [] allMethods = areaClass.getDeclaredMethods();
+    	for(Method method : allMethods) {
+    		if(method.getName().startsWith("check")) { //check
+    			check.add(method);
+    			System.out.println("Found one");
+    			System.out.println(allMethods);
+    		} else {
+    			System.out.println("no running methods with the prefic check found");
+    		}
+    	}
+    	System.out.println("Launcher test passed.");
+    }
+    
+    	
+
+    
+    
+    
     //Assertion#
     //Complex assertion
     //loggin
