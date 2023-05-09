2a6a6c80dbad89e3f3f7be06261aba650eb5c381
sanujakoralage <45386915+sanujakoralage@users.noreply.github.com>
2023-05-09 08:16:47 +0100

    Resilience reflection unit testing

--- src/src/reflectionPackage/reflectionTester.java
+++ src/src/reflectionPackage/reflectionTester.java
@@ -1,10 +1,36 @@
 package reflectionPackage;
 
+import java.lang.reflect.*;
 public class reflectionTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		reflectionOne area = new reflectionOne(4.0, 5.0);
+		 runResilienceTesting(area);
+ 
 	}
+	
+	//Reselience
+    public static void runResilienceTesting(reflectionOne rect) {
+    	
+    	// Class<?>[] areaClass= new Class<?>[] {reflectionOne.class};
+        
+        Class<?> areaClass = rect.getClass(); //object using array list
+        try {
+            Method method = areaClass.getDeclaredMethod("nonexistentMethod");
+            //method.equals()
+            method.setAccessible(true);
+            method.invoke(rect);
+            
+            //Exceptions
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (InvocationTargetException e) {
+            System.out.println("Resilience test passed."); //message
+        }
+    }
 
+    //launcher
+    //Assertion#
+    //Complex assertion
+    //loggin
 }
