c380f7bba08023ad4324df080b1ef2f5cca44c65
sanujakoralage <45386915+sanujakoralage@users.noreply.github.com>
2023-05-15 01:11:00 +0100

    reflection test class

--- src/src/portfolio4/reflection_Test.java
+++ src/src/portfolio4/reflection_Test.java
@@ -1,17 +1,23 @@
 package portfolio4;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-
 import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class reflection_Test {
     
+	
+	
+	
     private Area areaClass;
     
+    
+    
+ ///// Launcher unit testing////
     @Before
     public void setUp() {
         areaClass = new Area();
@@ -25,7 +31,6 @@ public class reflection_Test {
         
         for (Method methods : allMethods) {
             String methodName = methods.getName();
-            
             // if method starts with "check"
             if (methodName.startsWith("check")) {
                 try {
@@ -41,6 +46,73 @@ public class reflection_Test {
         if (!failedChecks.isEmpty()) {
             fail("The following check methods failed: " + failedChecks.toString());
         }
+    }
+    
+    
+    ////Resilience unit testing///
+    
+    @Test
+	public void testCalculateArea() {
+		Area area = new Area(4.0, 5.0);
+		assertEquals(20.0, area.calculateArea(), 0.0001);
+	}
+	
+	@Test
+	public void testDivideByZero() {
+		try {
+			double x = 4.0 / 2.0;
+			fail("Expected an Arithmetic Exception to be thrown");
+		} catch (ArithmeticException e) {
+			System.out.print("The test pass");
+		} catch (Exception e) {
+			fail("Unexpected exception: " + e.getMessage());
+		}
+	}
+	
+	
+	////Simple Assertion Unit Testing////
+	
+	@Test
+	public void testAreaCalculate() { //Area
+		Area area = new Area(3.0, 4.0);
+		double expectedArea = 12.0;
+		double actualArea = area.calculateArea();
+		assertEquals(expectedArea, actualArea, 0.0);
+	}
+	
+	@Test
+	public void testGetLength() { //Length
+		Area area = new Area(3.0, 4.0);
+		double expectedLength = 3.0;
+		double actualLength = area.getLength();
+		assertEquals(expectedLength, actualLength, 0.0);
+	}
+	
+	@Test
+	public void testGetWidth() { // Width
+		Area area = new Area(3.0, 4.0);
+		double expectedWidth = 4.0;
+		double actualWidth = area.getWidth();
+		assertEquals(expectedWidth, actualWidth, 0.0);
+	}
+	
+	
+	////Complex assertion unit testing////
+	
+	@Test
+    public void testArea() throws Exception {
+	    Area areaClass = new Area(3.0, 4.0);
+	    // Test if length is as expected
+	    Field fieldlength = areaClass.getClass().getDeclaredField("length");
+	    fieldlength.setAccessible(true);
+	    double lengthValue = (double) fieldlength.get(areaClass);
+	    assertEquals(3.0, lengthValue, 0.0);
+	    
+	    // Test if width is as expected
+	    Field fieldwidth = areaClass.getClass().getDeclaredField("width");
+	    fieldwidth.setAccessible(true);
+	    double widthValue = (double) fieldwidth.get(areaClass);
+	    assertEquals(4.0, widthValue, 0.0);
     }
     
 }
